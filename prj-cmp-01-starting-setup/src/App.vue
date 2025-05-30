55c8b65da527c71900792401f2ceaa5d63481f37
LeoPavlovski <leopavlovski01@gmail.com>
2025-05-30 19:56:00 +0200

    wip: Added Dummy Learning resources.

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<ul v-for="item in storedResources" :key="item.id">
+<li>
+  {{item}}
+</li>
+</ul>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      storedResources:[
+        {
+          id:'official-guide' , title:'Official Guide' , description:'The official Vue JS Documentation', link:'https://vuejs.org/guide/introduction'
+        },
+        {
+          id:'Google' , title:'Google' , description:'Learn to google..' , link:'https://google.com'
+        },
+      ]
+    }
+  }
+}
+</script>
