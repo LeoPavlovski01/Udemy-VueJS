f62f501d8628865f7baecc9855aa6bf4916f5008
LeoPavlovski <leopavlovski01@gmail.com>
2025-05-30 17:30:35 +0200

    Lesson 122 : Better Alert , using Dialog and Slots , instead of sending the props to the component.

--- cmp-adv-01-starting-setup/src/components/Alert.vue
+++ cmp-adv-01-starting-setup/src/components/Alert.vue
@@ -1,28 +1,27 @@
 <script>
 export default {
   name: "Alert.vue",
-  props:['text','color']
 }
 </script>
 
 <template>
-  <div :style="{backgroundColor: color === 'red' ? 'crimson' : 'green'}">
-    <p style="color:white;">This is the Alert</p>
-    <h2 style="color:white;">{{text}}</h2>
-  </div>
+  <dialog open>
+    <slot name="text"></slot>
+    <div>
+      <slot name="reason"></slot>
+    </div>
+  </dialog>
 </template>
 
 <style scoped>
-div {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+dialog{
+  margin:0;
+  position:fixed;
+  top:20vh;
+  left:30%;
+  width:40%;
+  background-color:white;
+  box-shadow:0 2px 8px rgba(0,0,0,0.26);
+  padding:1rem;
 }
 </style>
--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -4,7 +4,17 @@
     <input v-model="goal" type="text">
     <button @click="saveGoal">Add goal</button>
     <p>{{newGoal}}</p>
-    <alert v-if="error" :text="error" :color="'red'"></alert>
+    <alert v-if="invalidInput">
+      <template #text>
+        <div style="display:flex; align-items:center; justify-content: space-between">
+          <h2>Error!!</h2>
+          <button @click="closeDialog">X</button>
+        </div>
+      </template>
+      <template #reason>
+        <h2>{{errorText}}</h2>
+      </template>
+    </alert>
   </div>
 </template>
 
@@ -18,8 +28,10 @@ export default {
   data(){
     return{
       goal:'',
+      invalidInput:false,
       error:null,
       newGoal:null,
+      errorText:'',
       // goal ( with v-model )
     }
   },
@@ -29,14 +41,17 @@ export default {
     // }
   },
   methods:{
+    closeDialog(){
+      this.invalidInput = false;
+    },
     saveGoal(){
      if(this.goal === ''){
-      this.error = 'Input cant be empty';
-      this.newGoal = null;
+      this.invalidInput = true;
+      this.errorText= 'Invalid Input, Try Adding an Input!'
      }
      else{
-       this.error = null;
-       this.newGoal = this.goal;
+       this.invalidInput = false;
+       this.errorText = ''
      }
      // this.error = null;
     }
