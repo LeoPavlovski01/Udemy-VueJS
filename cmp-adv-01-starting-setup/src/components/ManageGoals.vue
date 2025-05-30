5f9fbf91726916fff898933a5ac3a42431e398fc
LeoPavlovski <leopavlovski01@gmail.com>
2025-05-30 17:06:25 +0200

    Creation of Alert Component.

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,16 +1,48 @@
-<script>
-export default {
-  name: "ManageGoals.vue"
-}
-</script>
-
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text">
+    <input v-model="goal" type="text">
+    <button @click="saveGoal">Add goal</button>
+    <p>{{newGoal}}</p>
+    <alert v-if="error" :text="error" :color="'red'"></alert>
   </div>
 </template>
 
-<style scoped>
+<script>
+import Alert from "@/components/Alert.vue";
+export default {
+  name: "ManageGoals.vue",
+  components:{
+    Alert
+  },
+  data(){
+    return{
+      goal:'',
+      error:null,
+      newGoal:null,
+      // goal ( with v-model )
+    }
+  },
+  watch:{
+    // goal(val){
+    //   console.log('Val : ' , val);
+    // }
+  },
+  methods:{
+    saveGoal(){
+     if(this.goal === ''){
+      this.error = 'Input cant be empty';
+      this.newGoal = null;
+     }
+     else{
+       this.error = null;
+       this.newGoal = this.goal;
+     }
+     // this.error = null;
+    }
+  },
+}
+</script>
 
+<style scoped>
 </style>
