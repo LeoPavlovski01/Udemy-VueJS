1f39ebc2817d80652d21e383cdae0ff758b6e37b
LeoPavlovski <leopavlovski01@gmail.com>
2025-07-31 15:05:17 +0200

    learning: How to fix 400, 500 status code , technical errors , DNS failure, no connection. Using the throw new Error Constructor to add a custom message if the status !ok.

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -9,7 +9,13 @@
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,21 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
+        <p v-if="error">
+          {{ error }}
+        </p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -38,32 +53,56 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
   methods: {
     submitSurvey() {
+      // Validation
       if (this.enteredName === '' || !this.chosenRating) {
         this.invalidInput = true;
         return;
       }
       this.invalidInput = false;
-      fetch('https://vue-http-demo-a77f1-default-rtdb.firebaseio.com/surveys.json' , {
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        },
-        body: JSON.stringify({
-          name:this.enteredName,
-          rating:this.chosenRating
+      this.error = null;
+      fetch(
+        'https://vue-http-demo-a77f1-default-rtdb.firebaseio.com/surveys.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          // When the request is wrong, it will go to the first .then case.
+          body: JSON.stringify({
+            name: this.enteredName,
+            rating: this.chosenRating,
+          }),
+          //   Status code.
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            // Handling 400 , 500 status code errors.
+            throw new Error('Could not save data...');
+          }
+          // technical errors catch. (endpoint PROBLEMS)
         })
-      })
+        //   CORS , No connection , DNS failure.
+        .catch((error) => {
+          // When for example the fetch method is /surveys and not /surveys.json this will end up here, because
+          // it's an issue with the url.
+          console.log('Error : ', error);
+          this.error = error.message;
+        });
+
       // this.$emit('survey-submit', {
       //   userName: this.enteredName,
       //   rating: this.chosenRating,
@@ -85,5 +124,4 @@ input[type='text'] {
   width: 20rem;
   margin-top: 0.5rem;
 }
-
 </style>
--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,14 +3,18 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button @click="getSurveys">Load Submitted Experiences</base-button>
+        <base-button @click="getSurveys"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-<!--      Basic loading-->
-      <p v-if="loading">Loading ...  please wait</p>
-<!--      If we have an catch error while fetching-->
-      <p v-else-if="!loading && errorMessage">{{errorMessage}}</p>
-      <p v-else-if="!loading && (!surveyResults || surveyResults.length === 0)">No Stored experiences found. Add some!</p>
-      <ul>
+      <!--      Basic loading-->
+      <p v-if="loading">Loading ... please wait</p>
+      <!--      If we have an catch error while fetching-->
+      <p v-else-if="!loading && errorMessage">{{ errorMessage }}</p>
+      <p v-else-if="!loading && (!surveyResults || surveyResults.length === 0)">
+        No Stored experiences found. Add some!
+      </p>
+      <ul v-else>
         <survey-result
           v-for="result in surveyResults"
           :key="result.id"
@@ -20,8 +24,8 @@
       </ul>
     </base-card>
   </section>
-<!--  <my-learning-survey @submit-survey="submitData"></my-learning-survey>-->
-<!--  <my-user-experiences :results="surveyResults"></my-user-experiences>-->
+  <!--  <my-learning-survey @submit-survey="submitData"></my-learning-survey>-->
+  <!--  <my-user-experiences :results="surveyResults"></my-user-experiences>-->
 </template>
 
 <script>
@@ -30,14 +34,14 @@ import SurveyResult from './SurveyResult.vue';
 // import MyUserExperiences from './MyUserExperiences.vue'
 
 export default {
-  data(){
-    return{
-      surveyResults:[],
-      loading:false,
-      errorMessage:null,
-    }
+  data() {
+    return {
+      surveyResults: [],
+      loading: false,
+      errorMessage: null,
+    };
   },
-  mounted(){
+  mounted() {
     this.getSurveys();
   },
   components: {
@@ -45,32 +49,37 @@ export default {
     // MyLearningSurvey,
     SurveyResult,
   },
-  methods:{
-    getSurveys(){
-      this.loading= true;
+  methods: {
+    getSurveys() {
+      this.loading = true;
       this.errorMessage = null;
-      fetch('https://vue-http-demo-a77f1-default-rtdb.firebaseio.com/surveys.json').then((response) => {
-      if(response.ok){
-        return response.json();
-      }
-     //  Using the arrow functions is better , cuz we cannot access this keywoard out of the scope.
-
-     }).then((data) =>{
-       this.loading = false;
-       const results = [];
-       // Transforming the objects to an array of objects so we can iterate through them.
-       for(const id in data){
-         results.push({
-           id:id,
-           name:data[id].name,
-           rating:data[id].rating,
-         })
-       }
-       this.surveyResults = results;
-     }).catch(()=>{
-       this.loading = false;
-       this.errorMessage = 'Failed to fetch the data, please try again later ...'
-      })
+      fetch(
+        'https://vue-http-demo-a77f1-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          //  Using the arrow functions is better , cuz we cannot access this keywoard out of the scope.
+        })
+        .then((data) => {
+          this.loading = false;
+          const results = [];
+          // Transforming the objects to an array of objects so we can iterate through them.
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.surveyResults = results;
+        })
+        .catch(() => {
+          this.loading = false;
+          this.errorMessage =
+            'Failed to fetch the data, please try again later ...';
+        });
     },
 
     // test(){
@@ -83,7 +92,7 @@ export default {
     //   }
     //   this.surveyResults.push(data);
     // }
-  }
+  },
 };
 </script>
 
